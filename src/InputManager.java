89c385da17e73245fe70e6929989d6fb2d2a5fcb
Johnny Huhtala <Animzor@gmail.com>
2016-02-03 13:08:53 +0100

    Input Manager implemented

--- src/InputManager.java
+++ src/InputManager.java
@@ -3,9 +3,22 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InputManager implements KeyListener, MouseListener, MouseMotionListener {
 
+	private int pressedKey;
+	
+	//Keys bound to actions
+	private HashMap<Integer, Action> bindings;
+	
+	//The objects that need to be notified when keys are pressed
+	private ArrayList<GameObject> observers;
+	
+	//Keeps track of which keys are down
+	private boolean[] keys = new boolean[4];
+	
 	private static InputManager thisInstance;
 	public static InputManager getInstance() {
 		if (thisInstance == null)
@@ -13,8 +26,50 @@ public class InputManager implements KeyListener, MouseListener, MouseMotionList
 		return thisInstance;
 	}
 	
+	public enum Action
+	{
+		UP, DOWN, LEFT, RIGHT
+	}
+	
 	private InputManager() {
 		//Constructor
+	}
+	
+	private boolean keyDown(int keyCode){
+		return keys[keyCode];
+	}
+	
+	public void addObserver(GameObject gObj){
+		observers.add(gObj);
+	}
+	
+	//The observers are notified
+	private void notifyObservers(Action action){
+
+		for(GameObject gObj : observers){
+//			gObj.notify(action);
+		}
+		
+	}
+	
+	public void initKeyBindings(){
+		bindings.put(KeyEvent.VK_UP, Action.UP);
+		bindings.put(KeyEvent.VK_DOWN, Action.DOWN);
+		bindings.put(KeyEvent.VK_LEFT, Action.LEFT);
+		bindings.put(KeyEvent.VK_RIGHT, Action.RIGHT);
+	}
+	
+	public void update(){
+
+		if(keyDown(pressedKey)){
+			if(bindings.get(pressedKey) == Action.UP ||
+					bindings.get(pressedKey) == Action.DOWN ||
+					bindings.get(pressedKey) == Action.LEFT ||
+					bindings.get(pressedKey) == Action.RIGHT){
+				notifyObservers(bindings.get(pressedKey));
+			}
+		}
+		
 	}
 	
 	@Override
@@ -33,10 +88,19 @@ public class InputManager implements KeyListener, MouseListener, MouseMotionList
 	public void mouseReleased(MouseEvent arg0) {}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {}
-
+	public void keyPressed(KeyEvent keyEvent) {
+		
+		keys[keyEvent.getKeyCode()] = true;	
+		update();
+		
+	}
+	
 	@Override
-	public void keyReleased(KeyEvent arg0) {}
+	public void keyReleased(KeyEvent keyEvent) {
+		
+		keys[keyEvent.getKeyCode()] = false;
+		
+	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {}
